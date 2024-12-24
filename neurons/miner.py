66d0b9ac6fbcd025a79cdd7f64f7a7447acd28c1
taogods <taogod@proton.me>
2024-12-24 10:53:22 -0800

    Fix default model bug

--- neurons/miner.py
+++ neurons/miner.py
@@ -34,7 +34,7 @@ from agentao.repo_environment import SUPPORTED_REPOS, REPO_TO_ENVIRONMENT_INFO
 
 class MinerDefaults:
     MAX_INSTANCE_COST = 3.
-    MODEL = "claude-3-5-sonnet"
+    MODEL = "gpt4omini"
 
 
 class Miner(BaseMinerNeuron):
