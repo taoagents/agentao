ec00efc646235e36dade7f6aad3e77f3193af635
taogods <taogod@proton.me>
2025-02-28 13:49:11 -0300

    Reduce num concurrent forwards and max miners per problem

--- neurons/constants.py
+++ neurons/constants.py
@@ -20,6 +20,6 @@ class ValidatorDefaults:
         min_files_to_consider_dir_for_problems=3,
         min_file_content_len=50,
     )
-    NEURON_NUM_CONCURRENT_FORWARDS: Final[int] = 3
-    MAX_MINERS_PER_PROBLEM: Final[int] = 20
+    NEURON_NUM_CONCURRENT_FORWARDS: Final[int] = 2
+    MAX_MINERS_PER_PROBLEM: Final[int] = 15
     NO_RESPONSE_MIN: Final[float] = 0.005
